a75cc92a7a597660a3a915a09cb3f3a50df41e1a
jeanyves226 <rjeanyvesbouda@gmail.com>
2024-08-08 17:14:03 +0000

    11 commit

--- business/views.py
+++ business/views.py
@@ -1,5 +1,5 @@
 import json
-from utils import sendWhatsAppMessage
+from .utils import sendWhatsAppMessage
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
